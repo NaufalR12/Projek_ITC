a5247de9d3ae24ded0b3073f079d5837de89d3f4
ajengrhhh <147266996+ajengrhhh@users.noreply.github.com>
2024-05-15 10:55:35 +0700

    Delete cari_pasien  directory

--- cari_pasien /js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("#loginButton").click(function () {
-    $("#loginModal").modal("show");
-  });
-
-  // Initialize tooltips
-  $('[data-toggle="tooltip"]').tooltip();
-});
