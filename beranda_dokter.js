d49726a64d8f61fb093fba83fba5d9f68f6b8e20
Shinta505 <142827052+Shinta505@users.noreply.github.com>
2024-05-18 22:30:49 +0700

    Add files via upload

---
+++ beranda_dokter.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#cariPasien").click(function () {
+        $("#loginModal").modal("show");
+    });
+
+    // Initialize tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+});
