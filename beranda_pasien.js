7b7aa28806b757f235e86dde2a5a26e825680746
Shinta505 <142827052+Shinta505@users.noreply.github.com>
2024-05-18 22:28:32 +0700

    Delete beranda_pasien.js

--- beranda_pasien.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#loginPasien").click(function () {
-      $("#loginModal").modal("show");
-    });
-  
-    // Initialize tooltips
-    $('[data-toggle="tooltip"]').tooltip();
-  });
