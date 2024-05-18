1afd42eeab07d63ed8c5aa8f336b169c541d30d0
Shinta505 <142827052+Shinta505@users.noreply.github.com>
2024-05-18 21:41:32 +0700

    Add files via upload

---
+++ beranda_pasien.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#loginPasien").click(function () {
+      $("#loginModal").modal("show");
+    });
+  
+    // Initialize tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+  });
